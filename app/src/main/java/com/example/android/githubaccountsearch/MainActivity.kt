e92c72b10da1d40f2ba3bd1e2faf4cac29150bc5
Birgit Schönauer <bschoenauer.mmt-b2016@fh-salzburg.ac.at>
2019-12-06 13:05:55 +0100

    chore: Add "material design" and initial daynight theme

--- app/src/main/java/com/example/android/githubaccountsearch/MainActivity.kt
+++ app/src/main/java/com/example/android/githubaccountsearch/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.android.githubaccountsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // debugging
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
